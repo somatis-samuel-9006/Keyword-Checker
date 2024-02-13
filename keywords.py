3893b7da1540c4088f6a5ce0944a57f2f88f4e0b
Samuel <ssg5689@gmail.com>
2024-02-13 00:43:52 -0800

    more words

--- keywords.py
+++ keywords.py
@@ -45,7 +45,8 @@ keywords = ['C++', 'c++', 'Git', 'git', 'HTML', 'Html', 'html', 'Java', 'java', 
              'Slack', 'Software Testing', 'software testing', 'Pytest', 'Bash', 'OpenMP', 'OpenCL', 'CUDA', 'OpenGL', 
              'Vulkan', 'shaders', 'Shaders', 'Produced', 'produced', 'Restructured', 'restructured', 'communicate', 'Communicate',
              'Problem Solving', 'Problem solving', 'problem solving', 'AI', 'Analytics', 'analytics', 'R', 'linux', 'DevOps', 'Solutions', 'solutions',
-            'platforms', 'Platforms', 'platform', 'Platforms', 'systems', 'Systems', 'framework', 'Framework', 'frameworks', 'Frameworks']
+            'platforms', 'Platforms', 'platform', 'Platforms', 'systems', 'Systems', 'framework', 'Framework', 'frameworks', 'Frameworks'
+            'machine learning', 'Machine learning', 'Qt']
 
 #takes in a multi-line string and builds a dict of all the keywords found in the job description string
 def create_keywords_table(job_desc):
